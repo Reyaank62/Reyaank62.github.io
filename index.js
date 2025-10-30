ae1a4ab6ebe5a1cb11c1c8d8e19d82408c685901
Reyaank62 <reyaan.kalsheker29@student.tamdistrict.org>
2025-10-30 17:37:57 +0000

    Added buttons that open a new webpage using javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("classlinks").onclick = function() {
+    window.location.href = "class_links.html";
+    };
+    
+document.getElementById("hobbies").onclick = function() {
+    window.location.href = "hobbies.html";
+    };
